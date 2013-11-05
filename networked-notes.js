a0b420e5c8c3af2be3f73cc695c67a8bbec53339
Jacob <2timesjay@gmail.com>
2013-11-04 18:45:55 -0800

    networked-notes.html/.js/.css added. New Layout

---
+++ networked-notes.js
@@ -0,0 +1,116 @@
+/*
+This is a basic graph example cribbed from bl.ocks.org
+It's a placeholder until an upgraded version of the dag.js 
+code gets substituted back in
+*/
+
+//Width and height
+      var w = 800;
+      var h = 400;
+
+      //Original data
+      var dataset = {
+        nodes: [
+          { name: "Adam", fixed: true},
+          { name: "Bob" },
+          { name: "Carrie", fixed: true },
+          { name: "Donovan" },
+          { name: "Edward" },
+          { name: "Felicity" },
+          { name: "George", fixed: true },
+          { name: "Hannah" },
+          { name: "Iris" },
+          { name: "Jerry" }
+        ],
+        edges: [
+          { source: 0, target: 1 },
+          { source: 0, target: 2 },
+          { source: 0, target: 3 },
+          { source: 0, target: 4 },
+          { source: 1, target: 5 },
+          { source: 2, target: 5 },
+          { source: 2, target: 5 },
+          { source: 3, target: 4 },
+          { source: 5, target: 8 },
+          { source: 5, target: 9 },
+          { source: 6, target: 7 },
+          { source: 7, target: 8 },
+          { source: 8, target: 9 }
+        ]
+      };
+
+      //Initialize a default force layout, using the nodes and edges in dataset
+      var force = d3.layout.force()
+                 .nodes(dataset.nodes)
+                 .links(dataset.edges)
+                 .size([w, h])
+                 .linkDistance([50])
+                 .charge([-100])
+                 .start();
+
+      var colors = d3.scale.category20();
+
+      //Create SVG element
+      var svg = d3.select("#graphic")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+      
+      //Create edges as lines
+      var edges = svg.selectAll("line")
+        .data(dataset.edges)
+        .enter()
+        .append("line")
+        .style("stroke", "#ccc")
+        .style("stroke-width", 1);
+      
+      //Create nodes as circles
+      var nodes = svg.selectAll(".node")
+                .data(dataset.nodes)
+        .enter()
+        .append("svg:g")
+                .attr("class","node")
+        .call(force.drag);
+      nodes.append("svg:circle")
+        .attr("r", 10)
+        .style("fill", function(d, i) {
+          return colors(i);
+        }).on("mouseover", function() {
+            d3.select(this)
+              .attr("r", 15);
+          }).on("mouseout", function() {
+           d3.select(this)
+              .transition()
+              .duration(500)
+            .attr("r", 10);
+          }).on("click", function(d, i) {
+                   var j = dataset.nodes.length;
+                   addNew(i, j, this)
+          });
+            nodes.append("text")
+                .attr('x', 0)
+                .attr('y', 4)
+                .attr('class', 'id')
+                .text(function(d) { console.log(d.name); return d.name ;  });
+
+            
+      //External node update; works with svg:g
+            var updateNode = function() {
+        this.attr("transform", function(d) {
+          return "translate("+d.x+","+ d.y+")";
+          });
+      }     
+
+      //Every time the simulation "ticks", this will be called
+      force.on("tick", function() {
+
+        edges.attr("x1", function(d) { return d.source.x; })
+           .attr("y1", function(d) { return d.source.y; })
+           .attr("x2", function(d) { return d.target.x; })
+           .attr("y2", function(d) { return d.target.y; });
+      
+        //nodes.attr("cx", function(d) { return d.x; })
+        //   .attr("cy", function(d) { return d.y; });
+                nodes.call(updateNode); 
+
+      });
